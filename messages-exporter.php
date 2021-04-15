2ed1193489e360d717d206873097e4d61e8ea4c6
Christopher Finke <chris@efinke.com>
2021-04-14 20:45:38 -0700

    Update messages-exporter.php

--- messages-exporter.php
+++ messages-exporter.php
@@ -1,7 +1,6 @@
 #!/usr/bin/env php
 <?php
 # Export Messages conversations to HTML files.
-# Enhanced by Garvin Hicking @supergarv https://garv.in
 # Based on https://github.com/PeterKaminski09/baskup, which was
 # based on https://github.com/kyro38/MiscStuff/blob/master/OSXStuff/iMessageBackup.sh
 #
