28179debcc3fb05f74fa5b593046c216105a7a9f
Christopher Finke <cfinke@gmail.com>
2023-01-27 20:44:10 +0000

    Adding meta tag for viewport.
    props to Dark Knight for the recommendation

--- messages-exporter.php
+++ messages-exporter.php
@@ -596,6 +596,7 @@ while ( $message = $messages->fetchArray() ) {
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Conversation: ' . $chat_title . '</title>
 		<style type="text/css">
 
